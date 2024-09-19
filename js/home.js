cdf44a054d1564a4ea79b846da06af45900cf7ea
fozlerabbi0 <fozlerabbi.me@gmail.com>
2024-09-19 11:44:27 +0600

    add the money added and input pin

--- js/home.js
+++ js/home.js
@@ -4,5 +4,12 @@ document.getElementById('btn-add-money')
 .addEventListener('click',function(event){
     event.preventDefault();
     console.log('Money add btn clicked');
+    // step-2:get money to be added to the account
+    const inputAddMoney=document.getElementById('input-add-money').value;
+    console.log(inputAddMoney);
+    
+    // get the pin number provided
+    const inputPinNumber = document.getElementById('input-pin-number').value;
+    console.log(inputPinNumber);
     
 });
